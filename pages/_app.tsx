326c2c2959e01dc5a2e985e4655c892c853413eb
besumero628 <besumero628@gmail.com>
2023-03-17 11:41:40 +0900

    GAの導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,10 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { HeaderLayout } from '@/components/templates/HeaderLayout'
 import theme from '@/utils/theme'
+import { usePageView } from '@/hooks/usePageView'
 
 export default function App({ Component, pageProps }: AppProps) {
+  usePageView()
   return (
     <ChakraProvider theme={theme}>
       <HeaderLayout />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,27 @@
+import { GA_ID } from '@/lib/gtag'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en" prefix='og: http://ogp.me/ns#'>
-      <Head />
+      <Head>
+      {GA_ID && (
+             <>
+               <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+               <script
+                 dangerouslySetInnerHTML={{
+                   __html: `
+                   window.dataLayer = window.dataLayer || [];
+                   function gtag(){dataLayer.push(arguments);}
+                   gtag('js', new Date());
+                   gtag('config', '${GA_ID}', {
+                     page_path: window.location.pathname,
+                   });`,
+                 }}
+               />
+             </>
+           )}
+      </Head>
       <body>
         <Main />
         <NextScript />
